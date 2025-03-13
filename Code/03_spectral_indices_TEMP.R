24080c19032aa9e4f0cfd07441fbefb6ccd7bb2f
filippomoratelli <73693941+filippomoratelli@users.noreply.github.com>
2025-03-13 15:45:56 +0100

    Update 03_spectral_indices_TEMP.R

--- Code/03_spectral_indices_TEMP.R
+++ Code/03_spectral_indices_TEMP.R
@@ -29,5 +29,34 @@ plot(mato2006[[1]], col=inferno(100))
 # values go from 0 to 255, why 255? images at 8 bits = 256 possible values (2^8)
 
 # DVI
-# Healthy tree: NIR=255, red=0, DVI = NIR-red = 255
-# Stressed tree: NIR = 100, red = 20, DVI = 80
+# Healthy tree: NIR = 255, red = 0, DVI = NIR-red = 255
+# Stressed tree: NIR = 100, red = 30, DVI = 70
+
+# Calculating DVI
+dvi1992 = mato1992[[1]] - mato1992[[2]] # NIR - red
+plot(dvi1992, col=magma(100)) # range from 255 to -255
+dvi2006 = mato2006[[1]] - mato2006[[2]]
+im.multiframe(1,2)
+plot(dvi1992, col=magma(100))
+plot(dvi2006, col=magma(100))
+
+# Different radiometric resolutions
+# DVI 8 bit: range from 255 to -255
+# DVI 4 bit: range from 15 to -15
+
+# standardized with Normalized DVI = (NIR - red) / (NIR + red)
+# range is always from 1 to -1
+
+# Calculating NDVI
+ndvi1992 = (mato1992[[1]] - mato1992[[2]]) / (mato1992[[1]] + mato1992[[2]])
+ndvi2006 = (mato2006[[1]] - mato2006[[2]]) / (mato2006[[1]] + mato2006[[2]])
+im.multiframe(1,2)
+plot(ndvi1992, col=mako(100))
+plot(ndvi2006, col=mako(100))
+
+# built-in functions from imageRy
+dvi1992auto = im.dvi(mato1992, 1, 2) # image, NIR band, red band
+dvi2006auto = im.dvi(mato2006, 1, 2)
+
+ndvi1992auto = im.ndvi(mato1992, 1, 2)
+ndvi2006auto = im.ndvi(mato2006, 1, 2)
