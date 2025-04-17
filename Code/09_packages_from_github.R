6fb24ea94b2670878d321679c31b1b9581082590
filippomoratelli <73693941+filippomoratelli@users.noreply.github.com>
2025-04-17 20:10:33 +0200

    Create 09_packages_from_github.R

---
+++ Code/09_packages_from_github.R
@@ -0,0 +1,12 @@
+# From GitHub:
+library(devtools)
+install_github("ducciorocchini/cblindplot")
+library(cblindplot)
+
+install_github("clauswilke/colorblindr")
+library(colorblindr)
+
+
+# From CRAN:
+install.packages("colorblindcheck")
+library(colorblindcheck)
