82057b0c0c736c2f7db200d76eb38d167e9f21ef
filippomoratelli <73693941+filippomoratelli@users.noreply.github.com>
2025-04-17 20:09:49 +0200

    Update 08_spatial_variability.R

--- Code/08_spatial_variability.R
+++ Code/08_spatial_variability.R
@@ -3,15 +3,19 @@
     # STANDARD DEVIATION
 # dataset con 4 età
 23, 22, 23, 49
+
   # MEDIA
 m = (23 + 22 + 23 + 49) / 4
 # m = 29.25
+
   # SCARTO QUADRATICO (numeratore)
 sq = (23-m)^2 + (22-m)^2 + (23-m)^2 + (49-m)^2
 # sq = 521
+
   # VARIANZA (sq/n dati)
 var = sq / 4
 # var = 130.25
+
 stdev = sqrt(var)
 # stdev = 11.40997
 
@@ -29,6 +33,7 @@ sent = im.import("sentinel.png")
 	# band 2 = red
 	# band 3 = green
 sent = flip(sent)
+
     # EXERCISE: plot the image in RGB with the NIR ontop of the red component
 im.plotRGB(sent, 1,2,3)
 
@@ -64,3 +69,71 @@ install.packages("RStoolbox")
 library(RStoolbox)
         p3 = ggRGB(sent, r=1, g=2, b=3)   
         p3 + p1 + p2
+
+
+	# what to do with HUGE IMAGES
+ncell(sent) * nlyr(sent)
+	# ncell = n pixel, nlyr = n layer (4 bande)
+
+senta = aggregate(sent, fact=2)
+	# aggregate(obj, fact = x = ogni x celle ne creo 1)
+
+ncell(senta) * nlyr(senta)
+	# viene un numero minore di prima
+
+senta5 = aggregate(sent, fact=5)
+
+	# EXERCISE make a multiframe and plot in RGB the three images (original, 2, 5)
+im.multiframe(1,3)
+	im.plotRGB(sent, 1, 2, 3)
+	im.plotRGB(senta, 1, 2, 3)
+	im.plotRGB(senta5, 1, 2, 3)
+
+	# Calculating standard deviation
+nira = senta[[1]]
+sd3a = focal(nira, w=c(3,3), fun="sd")
+plot(sd3a)
+
+	# EXERCISE calculate the standard deviation for the factor 5 image
+nira5 = senta5[[1]]
+sd3a5 = focal(nira5, w=c(3,3), fun="sd")
+
+sd5a5 = focal(nira5, w=c(5,5), fun="sd")
+plot(sd5a5)
+
+im.multiframe(1,2)
+plot(sd5a5)
+plot(sd3a)
+
+im.multiframe(2,2)
+plot(sd3)
+plot(sd3a)
+plot(sd3a5)
+plot(sd5a5)
+
+p1 = im.ggplot(sd3)
+p2 = im.ggplot(sd3a)
+p3 = im.ggplot(sd3a5)
+p4 = im.ggplot(sd5a5)
+
+p1 + p2 + p3 + p4
+
+im.multiframe(2,2)
+plot(sd3, col=mako(100))
+plot(sd3a, col=mako(100))
+plot(sd3a5, col=mako(100))
+plot(sd5a5, col=mako(100))
+
+	# Variance
+	# NIR 
+var3 = sd3^2
+plot(var3)
+
+im.multiframe(1,2)
+plot(sd3)
+plot(var3)
+
+sd5 = focal(nir, w=c(5,5), fun="sd")
+var5 = sd5^2
+plot(sd5)
+plot(var5)
