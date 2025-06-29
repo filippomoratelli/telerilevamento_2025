4555ca5d3f5e418ec303c2e21e8b91975e128000
filippomoratelli <73693941+filippomoratelli@users.noreply.github.com>
2025-06-29 12:50:42 +0200

    Create codice_esame.R

---
+++ esame/codice_esame.R
@@ -0,0 +1,84 @@
+    # work directory
+setwd("/Users/filippomoratelli/Desktop/UniBo/corsi/telerilevamento/esame")
+
+    # pacchetti necessari
+library(terra)
+library(imageRy)
+library(viridis)
+library(ggridges)
+
+    # importazione immagini
+cortina_19 = rast("cortina_19.tif") # importo le immagini
+cortina_25 = rast("cortina_25.tif")
+im.multiframe(1,2) # creo un pannello multiframe per due immagini affiancate (in RGB)
+plotRGB(cortina_19, r = 1, g = 2, b = 3, stretch = "lin", main = "Cortina, 2019") # plotto le immagini
+plotRGB(cortina_25, r = 1, g = 2, b = 3, stretch = "lin", main = "Cortina, 2025")
+dev.off() # chiudo
+
+    # PNG da salvare
+png("cortina_.png") # preparo il file png per salvare il plot
+im.multiframe(2,2) # creo un secondo multiframe per visualizzare anche il suolo nudo con banda NIR al posto del blu
+plotRGB(cortina_19, r = 1, g = 2, b = 3, stretch = "lin", main = "Cortina, 2019 (RGB)")
+plotRGB(cortina_25, r = 1, g = 2, b = 3, stretch = "lin", main = "Cortina, 2025 (RGB)")
+plotRGB(cortina_19, r = 1, g = 2, b = 4, stretch = "lin", main = "Cortina, 2019 (NIR)")
+plotRGB(cortina_25, r = 1, g = 2, b = 4, stretch = "lin", main = "Cortina, 2025 (NIR)")
+dev.off()
+
+    # DVI e NDVI
+dvi2019cortina = im.dvi(cortina_19, 4, 1) # calcolo il DVI (immagine, banda NIR, banda R)
+dvi2025cortina = im.dvi(cortina_25, 4, 1)
+ndvi2019cortina = im.ndvi(cortina_19, 4, 1) # calcolo il NDVI
+ndvi2025cortina = im.ndvi(cortina_25, 4, 1)
+png("cortina_dvi_ndvi.png")
+im.multiframe(2,2)
+plot(dvi2019cortina, main = "DVI Cortina, 2019")
+plot(dvi2025cortina, main = "DVI Cortina, 2025")
+plot(ndvi2019cortina, main = "NDVI Cortina, 2019")
+plot(ndvi2025cortina, main = "NDVI Cortina, 2025")
+dev.off()
+
+    # altre località - stesso procedimento
+san_vito_19 = rast("san_vito_19.tif")
+san_vito_25 = rast("san_vito_25.tif")
+tai_19 = rast("tai_19.tif")
+tai_25 = rast("tai_25.tif")
+
+png("san_vito.png", width = 480, height = 620)    # cambio le dimensioni in quanto le immagini sono troppo alte e non appare il testo
+im.multiframe(2,2)                 
+plotRGB(san_vito_19, r = 1, g = 2, b = 3, stretch = "lin", main = "San Vito di Cadore, 2019 (RGB)")
+plotRGB(san_vito_25, r = 1, g = 2, b = 3, stretch = "lin", main = "San Vito di Cadore, 2025 (RGB)")
+plotRGB(san_vito_19, r = 1, g = 2, b = 4, stretch = "lin", main = "San Vito di Cadore, 2019 (NIR)")
+plotRGB(san_vito_25, r = 1, g = 2, b = 4, stretch = "lin", main = "San Vito di Cadore, 2025 (NIR)")
+dev.off()
+
+png("tai.png")                      
+im.multiframe(2,2)                 
+plotRGB(tai_19, r = 1, g = 2, b = 3, stretch = "lin", main = "Tai di Cadore, 2019 (RGB)")
+plotRGB(tai_25, r = 1, g = 2, b = 3, stretch = "lin", main = "Tai di Cadore, 2025 (RGB)")
+plotRGB(tai_19, r = 1, g = 2, b = 4, stretch = "lin", main = "Tai di Cadore, 2019 (NIR)")
+plotRGB(tai_25, r = 1, g = 2, b = 4, stretch = "lin", main = "Tai di Cadore, 2025 (NIR)")
+dev.off()
+
+    # NDVI di Tai e San Vito
+ndvi2019sanvito = im.ndvi(san_vito_19, 4, 1)
+ndvi2025sanvito = im.ndvi(san_vito_25, 4, 1)
+ndvi2019tai = im.ndvi(tai_19, 4, 1)
+ndvi2025tai = im.ndvi(tai_25, 4, 1)
+png("sanvito_tai_ndvi.png")
+im.multiframe(2,2)
+plot(ndvi2019sanvito, main = "NDVI San Vito, 2019", col=viridis(100))
+plot(ndvi2025sanvito, main = "NDVI San Vito, 2025", col=viridis(100))
+plot(ndvi2019tai, main = "NDVI Tai, 2019", col=viridis(100))
+plot(ndvi2025tai, main = "NDVI Tai, 2025",col=viridis(100))
+dev.off()
+
+    # analisi multitemporale
+png("cortina_diff_green.png")
+cortina_diff = cortina_19[[2]] - cortina_25[[2]]        # calcolo differenza nella banda del verde tra 2019 e 2025
+plot(cortina_diff, main = "Cortina 2019-2025: differenza nella banda del verde")
+dev.off()
+
+png("cortina_diff_ndvi.png")
+cortina_diff_ndvi = ndvi2019cortina - ndvi2025cortina   # calcolo differenza NDVI
+plot(cortina_diff_ndvi, main = "Cortina 2019-2025: differenza di NDVI")
+dev.off()
