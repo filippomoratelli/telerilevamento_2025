b5534353b3fd634c293ce8db27331a431a4ef425
filippomoratelli <73693941+filippomoratelli@users.noreply.github.com>
2025-06-30 14:50:57 +0200

    Update codice_esame.R

--- esame/codice_esame.R
+++ esame/codice_esame.R
@@ -7,6 +7,7 @@ library(terra)
 library(imageRy)
 library(viridis)
 library(ggridges)
+library(patchwork)
 
 
     # importazione immagini
@@ -80,11 +81,9 @@ dev.off()
     # analisi multitemporale
 cortina_diff = cortina_19[[2]] - cortina_25[[2]]        # calcolo differenza nella banda del verde tra 2019 e 2025
 plot(cortina_diff, main = "Cortina 2019-2025: differenza nella banda del verde")
-dev.off()
 
 cortina_diff_ndvi = ndvi2019cortina - ndvi2025cortina   # calcolo differenza NDVI
 plot(cortina_diff_ndvi, main = "Cortina 2019-2025: differenza di NDVI")
-dev.off()
 
 png("cortina_diff.png")                                # creo png con multiframe verde + NDVI
 im.multiframe(1,2)
@@ -106,16 +105,18 @@ plotRGB(cortina_25, r = 1, g = 2, b = 3, stretch = "lin", main = "Cortina, 2025 
 crop_cortina = draw(x="extent", col="red", lwd=2)            # disegno un rettangolo sopra l'area di interesse
 cortina_25_crop = crop(cortina_25, crop_cortina)             # applico il crop alle due immagini originali e a quelle di ndvi
 cortina_19_crop = crop(cortina_19, crop_cortina)
+ndvi_19crop = crop(ndvi2019cortina, crop_cortina)
+ndvi_25crop = crop(ndvi2025cortina, crop_cortina)
 png("pistabob.png")
-im.multiframe(1,2)
+im.multiframe(2,2)
 plotRGB(cortina_19_crop, r = 1, g = 2, b = 3, stretch = "lin", main = "Pista da bob, 2019")
 plotRGB(cortina_25_crop, r = 1, g = 2, b = 3, stretch = "lin", main = "Pista da bob, 2025")
+plot(ndvi_19crop, main = "NDVI pista da bob, 2019")
+plot(ndvi_25crop, main = "NDVI pista da bob, 2025")
 dev.off()
 
 
         # ridgeline delle immagini ndvi croppate pista da bob
-ndvi_19crop = crop(ndvi2019cortina, crop_cortina)        # croppo le immagini ndvi
-ndvi_25crop = crop(ndvi2025cortina, crop_cortina)
 cortina_rl = c(ndvi_19crop, ndvi_25crop)                 # costruisco vettore con elementi le due immagini croppate ndvi
 names(cortina_rl) =c("NDVI 2019", "NDVI 2025")           
 png("ridgeline_bob.png")
@@ -126,13 +127,18 @@ dev.off()
         # classificazione immagini
 cortina_19_class = im.classify(ndvi_19crop, num_clusters=2)            # divido i pixel di ogni immagine in due cluster a seconda dei valori
 cortina_25_class = im.classify(ndvi_25crop, num_clusters=2)
-perc_19_c = freq(cortina_19_class) * 100 / ncell(cortina_19_class)     # calcolo la percentuale di ciascun cluster
-perc_19_c                                                              # visualizzo l'elemento appena creato
+png("classi_ndvi.png")                                                 # plotto le immagini con i pixel suddivisi nei due cluster per vedere come sono stati classificati
+im.multiframe(1,2)
+plot(cortina_19_class, main = "Pixel NDVI pista da bob, 2019")
+plot(cortina_25_class, main = "Pixel NDVI pista da bob, 2025")
+dev.off()
+perc_19_c = freq(cortina_19_class) * 100 / ncell(cortina_19_class)     # calcolo la frequenza percentuale di ciascun cluster
+perc_19_c                                                              # visualizzo la frequenza percentuale
           layer     value    count
     1 0.0192604 0.0192604 28.21649
     2 0.0192604 0.0385208 71.78351
 perc_25_c = freq(cortina_25_class) * 100 / ncell(cortina_25_class)
-perc_25_c
+perc_25_c                                                               
           layer     value count
     1 0.0192604 0.0192604  37.5
     2 0.0192604 0.0385208  62.5
@@ -149,3 +155,4 @@ ggplotcortina25 = ggplot(tabout, aes(x=NDVI, y=anno_2025, fill=NDVI, color=NDVI)
 png("ggplot_ndvi.png")
 ggplotcortina19 + ggplotcortina25 + plot_annotation(title = "Valori NDVI (% superficie) nel sito della pista da bob di Cortina")    # creo grafico con entrambi i ggplot creati
 dev.off()
+
